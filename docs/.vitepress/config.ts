02bfe90024ebdd5254c0dc84460200596aa42f50
hijiki51 <hibiki0719euph@gmail.com>
2023-05-18 00:58:09 +0900

    feat: 🎸 enable search

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -19,5 +19,32 @@ export default defineConfig({
     socialLinks: [
       { icon: "github", link: "https://github.com/traPtitech/naro-text" },
     ],
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "検索",
+            buttonAriaLabel: "検索ボックスを開く",
+          },
+          modal: {
+            displayDetails: "詳細を表示",
+            resetButtonTitle: "リセット",
+            backButtonTitle: "戻る",
+            noResultsText: "見つかりませんでした",
+            footer: {
+              selectText: "選択",
+              selectKeyAriaLabel: "結果を選択するには、上下キーを使用します",
+              navigateText: "移動",
+              navigateUpKeyAriaLabel:
+                "前の結果に移動するには、上キーを使用します",
+              navigateDownKeyAriaLabel:
+                "次の結果に移動するには、下キーを使用します",
+              closeText: "閉じる",
+            },
+          },
+        },
+      },
+    },
   },
 });
