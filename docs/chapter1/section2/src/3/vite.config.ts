b8515bb2b84e40d6cb5bc1b332adaddcc6b37ad8
mehm8128 <mehm8128@gmail.com>
2023-06-06 16:46:51 +0900

    色々直した

--- docs/chapter1/section2/src/3/vite.config.ts
+++ docs/chapter1/section2/src/3/vite.config.ts
@@ -1,6 +1,16 @@
-// ...
+import { fileURLToPath, URL } from "node:url"
+
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import vueJsx from "@vitejs/plugin-vue-jsx"
+
+// https://vitejs.dev/config/
 export default defineConfig({
-	// ...
-	base: "/TodoList/",
-	// ...
+	plugins: [vue(), vueJsx()],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+	base: "/<リポジトリ名>/", // [!code ++]
 })
