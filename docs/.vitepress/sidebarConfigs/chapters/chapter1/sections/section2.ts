869bab34470d620311ebc38a327bb5142eeb0ea7
mehm8128 <mehm8128@gmail.com>
2023-06-08 00:44:20 +0900

    タイトル変更

--- docs/.vitepress/sidebarConfigs/chapters/chapter1/sections/section2.ts
+++ docs/.vitepress/sidebarConfigs/chapters/chapter1/sections/section2.ts
@@ -2,21 +2,15 @@ import type { DefaultTheme } from 'vitepress'
 
 export const section2SidebarItems: DefaultTheme.SidebarItem[] = [
   {
-    text: 'Vue 入門',
-    link: '/chapter1/section2/index',
-    items: [
-      {
-        text: 'Vueに触れてみる',
-        link: '/chapter1/section2/1'
-      },
-      {
-        text: 'もっと触れてみる',
-        link: '/chapter1/section2/2'
-      },
-      {
-        text: '公開してみよう',
-        link: '/chapter1/section2/3'
-      }
-    ]
+    text: 'Vue入門',
+    link: '/chapter1/section2/1'
+  },
+  {
+    text: 'アプリを作ってみよう',
+    link: '/chapter1/section2/2'
+  },
+  {
+    text: '制作物を公開してみよう',
+    link: '/chapter1/section2/3'
   }
 ]
