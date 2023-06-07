af329323158a99ee2b1e6e8a26e765bcf804c3db
mehm8128 <mehm8128@gmail.com>
2023-06-07 19:53:59 +0900

    ソースコードをファイルに切り出した

---
+++ docs/chapter1/section2/src/2/ItemListExpensive.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<div>ItemList</div>
+		<ul>
+			<li
+				v-for="item in items"
+				:key="item.name"
+				:class="{ over500: item.price >= 500 }"
+			>
+				<div>名前: {{ item.name }}</div>
+				<div>{{ item.price }} 円</div>
+				<div v-if="item.price >= 10000">高額商品</div>
+			</li>
+		</ul>
+		==略==
+	</div>
+</template>
