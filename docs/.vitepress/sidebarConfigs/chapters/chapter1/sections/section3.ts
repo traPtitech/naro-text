002aef357e5ba3624a49b5973547ac4d2c31ab3f
pikachu0310 <sunnon@i.softbank.jp>
2023-06-09 16:45:57 +0900

    ファイルの整理

--- docs/.vitepress/sidebarConfigs/chapters/chapter1/sections/section3.ts
+++ docs/.vitepress/sidebarConfigs/chapters/chapter1/sections/section3.ts
@@ -2,23 +2,19 @@ import type { DefaultTheme } from 'vitepress'
 
 export const section3SidebarItems: DefaultTheme.SidebarItem[] = [
   {
-    text: 'サーバーを扱う準備',
-    link: '/chapter1/section3/1_prepare'
-  },
-  {
     text: 'サーバーアプリケーションを作る',
-    link: '/chapter1/section3/2_hello-server'
+    link: '/chapter1/section3/1_hello-server'
   },
   {
     text: 'JSONレスポンスを返す',
-    link: '/chapter1/section3/3_json-response'
+    link: '/chapter1/section3/2_json-response'
   },
   {
     text: 'ルーティング',
-    link: '/chapter1/section3/4_rooting'
+    link: '/chapter1/section3/3_rooting'
   },
   {
     text: 'サーバー演習問題',
-    link: '/chapter1/section3/5_server-exercise'
+    link: '/chapter1/section3/4_server-exercise'
   }
 ]
