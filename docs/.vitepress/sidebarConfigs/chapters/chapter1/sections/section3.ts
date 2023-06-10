e23ea48b77db97af56c10d356e56a482e0dd2a9d
pikachu0310 <sunnon@i.softbank.jp>
2023-06-10 13:03:25 +0900

    ファイル名を0-indexに変更

--- docs/.vitepress/sidebarConfigs/chapters/chapter1/sections/section3.ts
+++ docs/.vitepress/sidebarConfigs/chapters/chapter1/sections/section3.ts
@@ -3,18 +3,18 @@ import type { DefaultTheme } from 'vitepress'
 export const section3SidebarItems: DefaultTheme.SidebarItem[] = [
   {
     text: 'サーバーアプリケーションを作る',
-    link: '/chapter1/section3/1_hello-server'
+    link: '/chapter1/section3/0_hello-server'
   },
   {
     text: 'JSONレスポンスを返す',
-    link: '/chapter1/section3/2_json-response'
+    link: '/chapter1/section3/1_json-response'
   },
   {
     text: 'ルーティング',
-    link: '/chapter1/section3/3_rooting'
+    link: '/chapter1/section3/2_rooting'
   },
   {
     text: 'サーバー演習問題',
-    link: '/chapter1/section3/4_server-exercise'
+    link: '/chapter1/section3/3_server-exercise'
   }
 ]
