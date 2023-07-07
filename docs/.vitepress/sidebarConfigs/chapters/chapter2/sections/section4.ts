341ee2ee5f77644fafbe1d769a3eb0ea3f16ae45
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2023-07-07 18:25:43 +0900

    dockerコマンドについて

--- docs/.vitepress/sidebarConfigs/chapters/chapter2/sections/section4.ts
+++ docs/.vitepress/sidebarConfigs/chapters/chapter2/sections/section4.ts
@@ -1,3 +1,8 @@
 import type { DefaultTheme } from 'vitepress'
 
-export const section4SidebarItems: DefaultTheme.SidebarItem[] = []
+export const section4SidebarItems: DefaultTheme.SidebarItem[] = [
+  {
+    text: 'Hello, Docker',
+    link: '/chapter2/section4/0_commands'
+  }
+]
