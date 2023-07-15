a70c1fd8f3deb928bf45eda0f0ddac6560658ea1
nokhnaton <sage_of_forest@icloud.com>
2023-07-16 00:43:16 +0900

    プロキシまでの微修正

--- docs/.vitepress/sidebarConfigs/chapters/chapter2/sections/section2.ts
+++ docs/.vitepress/sidebarConfigs/chapters/chapter2/sections/section2.ts
@@ -1,6 +1,7 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const section2SidebarItems: DefaultTheme.SidebarItem[] = [
-  { text: 'vue-routerとプロキシの設定', link: '/chapter2/section2/1_router-setup' },
-  { text: 'エンドポイントにアクセスする', link: '/chapter2/section2/2_axios' }
+  { text: 'vue-routerシの設定', link: '/chapter2/section2/0_router-setup' },
+  { text: 'プロキシの設定', link: '/chapter2/section2/1_proxy' },
+  { text: 'エンドポイントにアクセスする', link: '/chapter2/section2/2_fetch' }
 ]
