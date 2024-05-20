fb1ccb8809add27a70b6444c659edc4f74f92f70
masky5859 <mathsuky2406@gmail.com>
2024-05-20 22:03:07 +0900

    コミットに関する文章を追加

---
+++ docs/.vitepress/sidebarConfigs/chapters/chapter1/dicts/commit-size.ts
@@ -0,0 +1,8 @@
+import type { DefaultTheme } from 'vitepress'
+
+export const commitSizeSidebarItems: DefaultTheme.SidebarItem[] = [
+  {
+    text: 'コミットについての説明',
+    link: '/chapter1/dicts/commit-size/0_index'
+  }
+]
--- docs/.vitepress/sidebarConfigs/chapters/chapter1/dicts/index.ts
+++ docs/.vitepress/sidebarConfigs/chapters/chapter1/dicts/index.ts
@@ -1,4 +1,8 @@
 import type { DefaultTheme } from 'vitepress'
 import { cleanCodeSidebarItems } from './clean-code'
+import { commitSizeSidebarItems } from './commit-size'
 
-export const dictSidebarItems: DefaultTheme.SidebarItem[] = [...cleanCodeSidebarItems]
+export const dictSidebarItems: DefaultTheme.SidebarItem[] = [
+  ...cleanCodeSidebarItems,
+  ...commitSizeSidebarItems
+]
